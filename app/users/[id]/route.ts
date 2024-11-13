e65ccd4398cb8a4a953a64ef0d7e7491f4a5b090
Ambition <ambikndsjobz@gmail.com>
2024-11-13 18:23:23 +0530

    adding route handler

---
+++ app/users/[id]/route.ts
@@ -0,0 +1,14 @@
+//dynamic route
+import { users } from "../route";
+
+export async function GET(
+    _request: Request,
+    {params}: {params: {id:string}}
+
+) {
+    const {id} = await params;
+    const user = users.find((user)=> user.id === parseInt(id))
+    return Response.json(user)
+}
+
+//add post put patch delete handler
